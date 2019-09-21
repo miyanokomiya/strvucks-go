cbd4e63479157c76fa97ad77015676eae8c3b9ed
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-09-21 17:18:45 +0900

    update

--- main.go
+++ main.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
 	"net/url"
 	"os"
 
 	"strvucks-go/internal/app/handler"
-	"strvucks-go/internal/app/model"
 
 	log "github.com/sirupsen/logrus"
 
@@ -36,7 +34,7 @@ func main() {
 	r.StaticFS("/assets", http.Dir("assets"))
 
 	r.GET("/exchange_token", func(c *gin.Context) {
-		exchangeToken(c)
+		handler.ExchangeToken(c)
 	})
 
 	r.GET("/webhooks", func(c *gin.Context) {
@@ -58,80 +56,4 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, `<p>Login by Strava</p>`)
 	fmt.Fprint(w, `<img src="/assets/strava.jpg" style="width: 120px; height: auto;" />`)
 	fmt.Fprint(w, `</a>`)
-}
-
-func exchangeToken(c *gin.Context) {
-	code := c.Query("code")
-
-	config := handler.Config()
-
-	token, err := config.Exchange(context.Background(), code)
-	if err != nil {
-		log.Error("Failure exchange token.")
-		c.String(400, "Failure exchange token.")
-		return
-	}
-
-	athlete, ok := token.Extra("athlete").(map[string]interface{})
-	if !ok {
-		log.Error("Failure get athlete from Strava response.")
-		c.String(400, "Failure get athlete from Strava response.")
-		return
-	}
-
-	idFloat, ok := athlete["id"].(float64)
-	if !ok {
-		log.Error("Failure get athlete from Strava response.")
-		c.String(400, "Failure get athlete from Strava response.")
-		return
-	}
-	id := int64(idFloat)
-
-	username, ok := athlete["username"].(string)
-	if !ok {
-		log.Error("Failure get athlete from Strava response.")
-		c.String(400, "Failure get athlete from Strava response.")
-		return
-	}
-
-	log.Info("Success get user from Strava response.", id, username)
-
-	permission := model.Permission{
-		AthleteID:    id,
-		AccessToken:  token.AccessToken,
-		TokenType:    token.TokenType,
-		RefreshToken: token.RefreshToken,
-		Expiry:       token.Expiry.Unix(),
-	}
-
-	db := model.DB()
-
-	user := model.User{}
-	if orm := db.Where("athlete_id = ?", id).First(&user); orm.Error == nil || orm.RecordNotFound() {
-		user.AthleteID = id
-		user.Username = username
-	} else {
-		log.Error("Failure get user.")
-		c.String(500, "Failure get user.")
-		return
-	}
-
-	tx := db.Begin()
-	tx = permission.Save(tx)
-	tx = user.Save(tx)
-
-	if err := tx.Error; err != nil {
-		tx.Rollback()
-		log.Error("Failure save token & user.")
-		c.String(500, "Failure save token & user.")
-		return
-	}
-
-	if err := tx.Commit().Error; err != nil {
-		log.Error("Failure save token & user.")
-		c.String(500, "Failure save token & user.")
-		return
-	}
-
-	c.Redirect(200, "/?auth=success")
 }
